71b54502ce7e3fe3e6349df61e725728b6c1d30d
mahfuzon <mahfuzon0@gmail.com>
2020-03-17 12:25:11 +0700

    menambahkan method logout di login controller

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class loginController extends Controller
 {
-    //
+    public function logout(){
+        Auth::logout();
+    }
 }
