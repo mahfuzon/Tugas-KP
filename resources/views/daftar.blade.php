00abb9bafbe2d19132ced499b5c7fdc0157ea398
mahfuzon <mahfuzon0@gmail.com>
2020-03-18 11:29:36 +0700

    revisi view daftar

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -23,9 +23,7 @@
 </head>
 
 <body class="bg-gradient-primary">
-
     <div class="container">
-
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
@@ -37,54 +35,39 @@
                                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                             </div>
                             <form class="user">
-                                <div class="form-group row">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                            placeholder="First Name">
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                            placeholder="Last Name">
-                                    </div>
+                                <div class="form-group">
+                                <label for="nama">Nama</label>
+                                    <input type="text" class="form-control form-control-user" id="nama" name="nama">
                                 </div>
                                 <div class="form-group">
-                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                        placeholder="Email Address">
+                                <label for="asal_sekolah">Asal Sekolah</label>
+                                    <input type="text" class="form-control form-control-user" id="asal_sekolah" name="asal_sekolah">
+                                </div>
+                                <div class="form-group">
+                                <label for="email">Email</label>
+                                    <input type="email" class="form-control form-control-user" id="email" name="email">
                                 </div>
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input type="password" class="form-control form-control-user"
-                                            id="exampleInputPassword" placeholder="Password">
+                                    <label for="mulai">Tanggal Mulai</label>
+                                        <input type="date" class="form-control form-control-user"
+                                            id="mulai" name="mulai">
                                     </div>
                                     <div class="col-sm-6">
-                                        <input type="password" class="form-control form-control-user"
-                                            id="exampleRepeatPassword" placeholder="Repeat Password">
+                                    <label for="selesai">Tanggal Selesai</label>
+                                        <input type="date" class="form-control form-control-user"
+                                            id="selesai" neme="selesai">
                                     </div>
                                 </div>
                                 <a href="login.html" class="btn btn-primary btn-user btn-block">
                                     Register Account
                                 </a>
-                                <hr>
-                                <a href="index.html" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Register with Google
-                                </a>
-                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                                </a>
                             </form>
-                            <hr>
-                            <div class="text-center">
-                                <a class="small" href="forgot-password.html">Forgot Password?</a>
-                            </div>
-                            <div class="text-center">
-                                <a class="small" href="login.html">Already have an account? Login!</a>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 
     <!-- Bootstrap core JavaScript-->
