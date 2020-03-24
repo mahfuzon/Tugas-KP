549a7858c06dd37c84e445d96b97163c68c1938d
mahfuzon <mahfuzon0@gmail.com>
2020-03-24 10:58:46 +0700

    sebelum migrasi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('level');
             $table->string('password');
+            $table->date('mulai');
+            $table->date('selesai');
             $table->rememberToken();
             $table->timestamps();
         });
