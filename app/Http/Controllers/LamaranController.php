bb4af2844d4b8eb33ea39c3b9c81fe698a93fd4f
mahfuzon <mahfuzon0@gmail.com>
2020-04-01 17:58:30 +0700

    kondisi guru login

--- app/Http/Controllers/LamaranController.php
+++ app/Http/Controllers/LamaranController.php
@@ -63,33 +63,76 @@ class LamaranController extends Controller
         $mulai = strtotime($request->mulai);
         $selesai = strtotime($request->selesai);
         $selisih = ($selesai - $mulai)/86400;
-        if($mulai_daftar < $date || $selesai_kp < $date){
-            Session::flash('flash_message', 'inputkan tanggal yang sesuai');
-            return redirect('/daftar')->withInput();
-        }else if($selisih < 60){  
-            Session::flash('flash_message', 'Magang minimal 2 bulan');
-            return redirect('/daftar')->withInput();
+        if(auth()->user()->level == 'guru'){
+            if($mulai_daftar < $date || $selesai_kp < $date){
+                Session::flash('flash_message', 'inputkan tanggal yang sesuai');
+                return redirect('/daftar')->withInput();
+            }else if($selisih < 60){  
+                Session::flash('flash_message', 'Magang minimal 2 bulan');
+                return redirect('/daftar')->withInput();
+            }else{
+                $lampiran1 = new lampiran;
+                $cv1 = $request->file('cv1');
+                $extensi1 = $c1->getClientOriginalExtension();
+                $nama1 = date('Y-M-d-H-i-s'). " $request->nama1". ".$extensi1";
+                $penyimpanan1 = 'cv_peserta';
+                $cv1->move($penyimpanan1, $nama1);
+                $lampiran1->cv = $nama1;
+
+                $lampiran1->nama = $request->nama1;
+                $lampiran1->asal_sekolah = $request->asal_sekolah;
+                $lampiran1->email = $request->email1;
+                $lampiran1->mulai = $request->mulai;
+                $lampiran1->selesai = $request->selesai;  
+                $lampiran1->acc = 0;
+                $lampiran1->save();
+
+                $lampiran2 = new lampiran;
+                // cv
+                $cv2 = $request->file('cv2');
+                $extensi2 = $cv->getClientOriginalExtension();
+                $nama2 = date('Y-M-d-H-i-s'). " $request->nama2". ".$extensi2";
+                $penyimpanan2 = 'cv_peserta';
+                $cv2->move($penyimpanan2, $nama2);
+                $lampiran2->cv = $nama2;
+
+                $lampiran2->nama = $request->nama2;
+                $lampiran2->asal_sekolah = $request->asal_sekolah;
+                $lampiran2->email = $request->email2;
+                $lampiran2->mulai = $request->mulai;
+                $lampiran2->selesai = $request->selesai;  
+                $lampiran2->acc = 0;
+                $lampiran2->save();
+            }    
         }else{
-            $lampiran = new lampiran;
+            if($mulai_daftar < $date || $selesai_kp < $date){
+                Session::flash('flash_message', 'inputkan tanggal yang sesuai');
+                return redirect('/daftar')->withInput();
+            }else if($selisih < 60){  
+                Session::flash('flash_message', 'Magang minimal 2 bulan');
+                return redirect('/daftar')->withInput();
+            }else{
+                $lampiran = new lampiran;
 
-            // cv
-            $cv = $request->file('cv');
-            $extensi = $cv->getClientOriginalExtension();
-            $nama = date('Y-M-d-H-i-s'). " $request->nama". ".$extensi";
-            $penyimpanan = 'cv_peserta';
-            $cv->move($penyimpanan, $nama);
-            $lampiran->cv = $nama;
+                // cv
+                $cv = $request->file('cv');
+                $extensi = $cv->getClientOriginalExtension();
+                $nama = date('Y-M-d-H-i-s'). " $request->nama". ".$extensi";
+                $penyimpanan = 'cv_peserta';
+                $cv->move($penyimpanan, $nama);
+                $lampiran->cv = $nama;
 
 
-            $lampiran->nama = $request->nama;
-            $lampiran->asal_sekolah = $request->asal_sekolah;
-            $lampiran->email = $request->email;
-            $lampiran->mulai = $request->mulai;
-            $lampiran->selesai = $request->selesai;  
-            $lampiran->acc = 0;
-            $lampiran->save();
+                $lampiran->nama = $request->nama;
+                $lampiran->asal_sekolah = $request->asal_sekolah;
+                $lampiran->email = $request->email;
+                $lampiran->mulai = $request->mulai;
+                $lampiran->selesai = $request->selesai;  
+                $lampiran->acc = 0;
+                $lampiran->save();
+            }
+            return redirect('/login');
         }
-        return redirect('/login');
     }
 
     public function download($id)
