b160e825084d27bb715e982940107e1149a7e703
mahfuzon <mahfuzon0@gmail.com>
2020-05-20 05:37:18 +0700

    tambah sekolah_id pada model peserta

--- app/peserta.php
+++ app/peserta.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class peserta extends Model
 {
     protected $table = "peserta";
-    protected $fillable = ['lampiran_id'];
+    protected $fillable = ['lampiran_id', 'sekolah_id'];
 
     public function lampiran(){
         return $this->belongsTo('App\lampiran','lampiran_id');
