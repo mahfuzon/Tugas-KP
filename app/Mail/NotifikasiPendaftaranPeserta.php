17551b783bf675f006ea7c5a9e6fb1d3093b4369
mahfuzon <mahfuzon0@gmail.com>
2020-05-31 10:21:13 +0700

    NotifikasiPendaftaranPeserta line 31

--- app/Mail/NotifikasiPendaftaranPeserta.php
+++ app/Mail/NotifikasiPendaftaranPeserta.php
@@ -28,6 +28,6 @@ class NotifikasiPendaftaranPeserta extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.NotifPendaftaranPeserta');
     }
 }
